c36fd3cf45784bb156720e75006db98702b1df79
João Gabriel <main.joaogabrielsg@gmail.com>
2021-08-25 08:52:43 -0300

    :sparkles: feat: add give classes and study in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
 import { Switch, Route } from 'react-router-dom';
 
 import { Home } from '../pages/Home';
+import { Study } from '../pages/Study';
+import { GiveClasses } from '../pages/GiveClasses';
 
 export function Routes() {
   return (
     <Switch>
       <Route path="/" component={Home} exact />
+      <Route path="/study" component={Study} />
+      <Route path="/give-classes" component={GiveClasses} />
     </Switch>
   );
 }
