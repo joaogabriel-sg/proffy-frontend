267b31fc8007766c6f3ece3465ce8bbf1ace24a8
João Gabriel <main.joaogabrielsg@gmail.com>
2021-08-26 08:15:07 -0300

    :sparkles: feat: create wrapper component

---
+++ src/components/Wrapper/index.tsx
@@ -0,0 +1,12 @@
+import { PropsWithChildren, ReactNode } from 'react';
+import { Container } from './styles';
+
+type WrapperProps = PropsWithChildren<ReactNode>;
+
+export function Wrapper({ children }: WrapperProps) {
+  return (
+    <Container>
+      {children}
+    </Container>
+  );
+}
---
+++ src/components/Wrapper/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  padding: 0 0.8rem 2rem;
+  margin-top: -6.4rem;
+`;
