da93b02e3d2c2b2c1c45f7de5a4abee39024e892
João Gabriel <main.joaogabrielsg@gmail.com>
2021-08-25 22:45:29 -0300

    :sparkles: feat: add school subject and weekdays helpers

---
+++ src/helpers/schoolSubjects.ts
@@ -0,0 +1,46 @@
+export const schoolSubjects = [
+  {
+    schoolSubject: 'Matemática',
+    value: 'math',
+  },
+  {
+    schoolSubject: 'Português',
+    value: 'portuguese',
+  },
+  {
+    schoolSubject: 'Química',
+    value: 'chemistry',
+  },
+  {
+    schoolSubject: 'Física',
+    value: 'physics',
+  },
+  {
+    schoolSubject: 'Espanhol',
+    value: 'spanish',
+  },
+  {
+    schoolSubject: 'Inglês',
+    value: 'english',
+  },
+  {
+    schoolSubject: 'Filosofia',
+    value: 'philosophy',
+  },
+  {
+    schoolSubject: 'Sociologia',
+    value: 'sociology',
+  },
+  {
+    schoolSubject: 'Geografia',
+    value: 'geography',
+  },
+  {
+    schoolSubject: 'Ciências',
+    value: 'science',
+  },
+  {
+    schoolSubject: 'Educação Física',
+    value: 'physical-education',
+  },
+];
---
+++ src/helpers/weekdays.ts
@@ -0,0 +1,30 @@
+export const weekdays = [
+  {
+    weekday: 'Segunda-feira',
+    value: 'monday',
+  },
+  {
+    weekday: 'Terça-feira',
+    value: 'tuesday',
+  },
+  {
+    weekday: 'Quarta-feira',
+    value: 'wednesday',
+  },
+  {
+    weekday: 'Quinta-feira',
+    value: 'thursday',
+  },
+  {
+    weekday: 'Sexta-feira',
+    value: 'friday',
+  },
+  {
+    weekday: 'Sábado',
+    value: 'saturday',
+  },
+  {
+    weekday: 'Domingo',
+    value: 'sunday',
+  },
+];
