919f9f9417c36cff06bdb189cc6fde40984f5715
João Gabriel <main.joaogabrielsg@gmail.com>
2021-08-25 21:53:12 -0300

    :sparkles: feat: create fieldset component

---
+++ src/components/Fieldset/index.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren, ReactNode } from 'react';
+import { Container } from './styles';
+
+type FieldsetProps = {
+  legend: string | ReactNode;
+} & PropsWithChildren<ReactNode>
+
+export function Fieldset({ legend, children }: FieldsetProps) {
+  return (
+    <Container>
+      <legend>{legend}</legend>
+      {children}
+    </Container>
+  );
+}
