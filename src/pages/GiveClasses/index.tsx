880d9b01f01aadbbf6776ff8d0b979fdae339c1b
João Gabriel <main.joaogabrielsg@gmail.com>
2021-08-26 21:41:33 -0300

    :recycle: refactor: import newProffySchema

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -13,6 +13,7 @@ import warningIcon from '../../assets/warning.svg';
 
 import { schoolSubjects } from '../../helpers/schoolSubjects';
 import { weekdays } from '../../helpers/weekdays';
+import { newProffySchema } from '../../schemas/newProffySchema';
 
 import {
   Container,
@@ -25,46 +26,13 @@ import {
   Button,
 } from './styles';
 
-const NewProffySchema = z.object({
-  name: z.string().nonempty({ message: 'Seu nome completo é fundamental em nossa plataforma.' }),
-  avatar: z
-    .string()
-    .nonempty({ message: 'Coloque o link da sua foto para que os alunos o conheçam.' })
-    .url({ message: 'O link da imagem é inválido, tente utilizar um link como http://meusite.com/minha-imagem.jpg' }),
-  whatsapp: z
-    .string()
-    .nonempty({ message: 'O WhatsApp é um excelente meio para acolher possíveis alunos, lembre-se de preencher.' }),
-  bio: z
-    .string()
-    .nonempty({ message: 'Invista bastante numa boa biografia para atrair a atenção dos alunos.' }),
-  schoolSubject: z
-    .string()
-    .nonempty({ message: 'Eita, parece que você esqueceu de escolher a matéria que lecionará.' }),
-  price: z
-    .number()
-    .positive({ message: 'Não se esqueça do preço da sua hora de aula.' }),
-  weekday: z.string().nonempty({ message: 'Parece que você não selecionou o dia da aula.' }),
-  hoursFrom: z
-    .number()
-    .nonnegative({ message: 'A hora está errada, use horários entre 0 e 23.' })
-    .min(0)
-    .max(23, { message: 'Um momento, a hora máxima é 23h.' })
-    .int({ message: 'Infelizmente não é possível usar números "quebrados".' }),
-  hoursTo: z
-    .number()
-    .nonnegative({ message: 'A hora está errada, use horários entre 0 e 23.' })
-    .min(0)
-    .max(23, { message: 'Um momento, a hora máxima é 23h.' })
-    .int({ message: 'Infelizmente não é possível usar números "quebrados".' }),
-});
-
-type NewProffySchemaType = z.infer<typeof NewProffySchema>
+type NewProffySchemaType = z.infer<typeof newProffySchema>
 
 export function GiveClasses() {
   const {
     register, handleSubmit, formState: { errors },
   } = useForm<NewProffySchemaType>({
-    resolver: zodResolver(NewProffySchema),
+    resolver: zodResolver(newProffySchema),
   });
 
   const handleSubmitNewProffy: SubmitHandler<NewProffySchemaType> = (data) => {
