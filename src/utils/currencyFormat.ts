2da8701cf428b3eb622e72889079f1294d6d2b6f
João Gabriel <main.joaogabrielsg@gmail.com>
2021-08-30 10:45:20 -0300

    :sparkles: feat: add currency format util

---
+++ src/utils/currencyFormat.ts
@@ -0,0 +1,9 @@
+export function currencyFormat(currency: number) {
+  return new Intl.NumberFormat(
+    'pt-BR',
+    {
+      style: 'currency',
+      currency: 'BRL',
+    },
+  ).format(currency);
+}
