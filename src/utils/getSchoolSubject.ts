df72c7388c48db7044910a15dcc439db2c5feab1
João Gabriel <main.joaogabriel@gmail.com>
2021-09-04 12:17:11 -0300

    :sparkles: add get school subject util

---
+++ src/utils/getSchoolSubject.ts
@@ -0,0 +1,5 @@
+import { schoolSubjects } from '../mocks/schoolSubjects';
+
+export function getSchoolSubject(name: string) {
+  return schoolSubjects.find(({ value }) => value === name)?.schoolSubject || '';
+}
