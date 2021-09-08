3430902582b700f8101f8f16374199ee05426a10
João Gabriel <main.joaogabrielsg@gmail.com>
2021-09-08 18:29:54 -0300

    :sparkles: feat: add motion animations to wrapper component

--- src/components/Wrapper/styles.ts
+++ src/components/Wrapper/styles.ts
@@ -1,6 +1,27 @@
 import styled from 'styled-components';
+import { motion, Variants } from 'framer-motion';
 
-export const Container = styled.section`
+const containerVariants: Variants = {
+  hidden: {
+    y: 40,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1.5,
+      type: 'spring',
+      bounce: 0.65,
+    },
+  },
+};
+
+export const Container = styled(motion.section).attrs(() => ({
+  variants: containerVariants,
+  initial: 'hidden',
+  animate: 'visible',
+}))`
   padding: 0 0.8rem 2rem;
   margin-top: -6.4rem;
 `;
