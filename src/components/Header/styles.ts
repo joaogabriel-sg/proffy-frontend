bbc6270dcc155459d21891ced7e6b603a1e4c00a
João Gabriel <main.joaogabrielsg@gmail.com>
2021-09-08 18:16:33 -0300

    :sparkles: feat: add motion animations to header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { motion, Variants } from 'framer-motion';
 
 export const Container = styled.header`
   background: var(--color-primary);
@@ -7,11 +8,29 @@ export const Container = styled.header`
   padding: 1.2rem 0.8rem;
 `;
 
-export const Content = styled.div`
+const contentVariants: Variants = {
+  hidden: {
+    y: -100,
+  },
+  visible: {
+    y: 0,
+    transition: {
+      duration: 1.25,
+      type: 'spring',
+      bounce: 0.6,
+    },
+  },
+};
+
+export const Content = styled(motion.div).attrs(() => ({
+  variants: contentVariants,
+  initial: 'hidden',
+  animate: 'visible',
+}))`
   width: 100%;
 `;
 
-export const Menu = styled.div`
+export const Menu = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
